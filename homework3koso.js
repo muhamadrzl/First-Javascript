12899fef9b9fa2cb155b31914ffd2a520c71b234
Muhamad Rizal Ridlo Prakoso <84617244+muhamadrzl@users.noreply.github.com>
2023-01-28 23:30:53 +0700

    Add files via upload

--- homework3koso.js
+++ homework3koso.js
@@ -65,8 +65,55 @@ function compareArrays(odd, even){
     let tot_even = total(even)
     let avg_odd = average(odd)
     let avg_even = average(even)
+
     console.log(`Array_Odd : min =  ${min_odd} max = ${max_odd} total = ${tot_odd} average =${avg_odd} `)
     console.log(`Array_Even : min =  ${min_even} max = ${max_even} total = ${tot_even} average =${avg_even} `)
+    
+    if (min_odd<min_even)
+    {
+        console.log('min_even lebih besar dibanding min_odd')
+    }
+    else if (min_odd>min_even){
+        console.log('min_even lebih kecil dibanding min_odd')
+    }
+    else{
+        console.log ('min_even sama dengan dibanding min_odd')
+    }
+
+    if (max_odd<max_even)
+    {
+        console.log('max_even lebih besar dibanding max_odd')
+    }
+    else if (max_odd>max_even){
+        console.log('max_even lebih kecil dibanding max_odd')
+    }
+    else{
+        console.log ('max_even sama dengan dibanding max_odd')
+    }
+
+    if (tot_odd<tot_even)
+    {
+        console.log('tot_even lebih besar dibanding tot_odd')
+    }
+    else if (tot_odd>tot_even){
+        console.log('tot_even lebih kecil dibanding tot_odd')
+    }
+    else{
+        console.log ('tot_even sama dengan dibanding tot_odd')
+    }
+
+    if (avg_odd<avg_even)
+    {
+        console.log('avg_even lebih besar dibanding avg_odd')
+    }
+    else if (avg_odd>avg_even){
+        console.log('avg_even lebih kecil dibanding avg_odd')
+    }
+    else{
+        console.log ('avg_even sama dengan dibanding avg_odd')
+    }
+ 
+
 }
 let arr = randomArray()
 console.log(arr)
